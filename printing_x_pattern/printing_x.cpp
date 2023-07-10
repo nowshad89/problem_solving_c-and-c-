0cbd03c2c57a5a7af80f7a0df0937ec8bbf8b526
Nowshad91 <nowshadmahmud90@gmail.com>
2023-07-11 00:24:12 +0600

    removing unnecessary code for pattern printing

--- printing_x_pattern/printing_x.cpp
+++ printing_x_pattern/printing_x.cpp
@@ -52,29 +52,3 @@ int main()
 }
 
 
-// #include <iostream>
-
-// using namespace std;
-
-// int main() {
-//     int N = 5;  // Number of rows in the pattern
-
-//     for (int i = 0; i < N; i++) {
-//         for (int j = 0; j < N; j++) {
-//             if (i == j || i + j == N - 1) {
-//                 cout << "\\";
-//             } else if (i < j && i + j < N - 1) {
-//                 cout << " ";
-//             } else if (i > j && i + j > N - 1) {
-//                 cout << " ";
-//             } else {
-//                 cout << " ";
-//             }
-//         }
-//         cout << endl;
-//     }
-
-//     return 0;
-// }
-
-
